6e56db089e810ab44d209615521ade847907ad30
alrifat241106 <alrifat.241106@s.pust.ac.bd>
2026-07-25 12:11:41 +0600

    Add files via upload

---
+++ Counter of even number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, count = 0;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    for(i = 2; i <= n; i += 2)
+    {
+        count++;
+    }
+
+    printf("Number of even numbers = %d\n", count);
+
+    return 0;
+}
